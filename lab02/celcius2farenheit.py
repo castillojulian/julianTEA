8a3c7819fac9e32feffd8810e31fa643daf3445b
Julian David Castillo Araya (24275) <julian.castillo@est.zamorano.edu>
2022-09-12 23:09:53 -0600

    Version final lab02 TEA

---
+++ lab02/celcius2farenheit.py
@@ -0,0 +1,5 @@
+#Tendencias e Innovacion en Tecnologia Agricola
+temp_celcius = float(input("Cual es la temperatura en celcius? "))
+temp_farenheit = (temp_celcius * 9/5) + 32
+
+print("Temperatura en Farenheit = " + str(temp_farenheit))
